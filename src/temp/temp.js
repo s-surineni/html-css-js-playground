93f894410abc2bf0d009d9a9d1fe201b6c9af9e5
ssurineni <ssurineni@salesforce.com>
2026-05-13 07:01:40 +0530

    added code for throttling

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -1,42 +1,40 @@
-const button = document.querySelector(".add-btn");
-const wrapper = document.querySelector(".wrapper");
-const MAX_LIMIT = 3;
-button.addEventListener("click", handleButtonClick)
+const addButton = document.querySelector(".add-progress-bar");
+addButton.addEventListener("click", handleButtonClick);
 const que = [];
 let activeCount = 0;
-
+const MAX_LIMIT = 3;
 function handleButtonClick() {
-    const bar = createProgressBarUI();
-    que.push(() => startTrans(bar))
+    const progressFill = createProgressUI();
+    que.push(() => startTrans(progressFill))
     processQueue();
 }
+
 function processQueue() {
     if (activeCount < MAX_LIMIT && que.length > 0) {
         activeCount++;
         const startTrans = que.shift();
         startTrans();
     }
-
 }
 
-function startTrans(bar) {
-      requestAnimationFrame(() => {
-    bar.style.width = '100%';
-  });
+function startTrans(progressFill) {
+    requestAnimationFrame(() => {
+        progressFill.style.width = "100%";
+    })
 
-    bar.addEventListener('transitionend', () => {
-    activeCount--;
-    processQueue(); // Slot opened up! Check the queue.
-  }, { once: true });
+    progressFill.addEventListener('transitionend', ()=> {
+        activeCount--;
+        processQueue();
+    }, { once: true })
 }
-function createProgressBarUI() {
-    const progressContainer = document.createElement('div');
-    progressContainer.className= "progress-container";
 
-    const progressFill = document.createElement('div');
-    progressFill.className= "progress-fill";
+function createProgressUI() {
+    const progressBar = document.createElement("div");
+    progressBar.className = "progress-bar";
+    document.body.appendChild(progressBar);
 
-    wrapper.appendChild(progressContainer);
-    progressContainer.appendChild(progressFill);
+    const progressFill = document.createElement("div");
+    progressFill.className = "progress-fill";
+    progressBar.appendChild(progressFill);
     return progressFill;
 }
