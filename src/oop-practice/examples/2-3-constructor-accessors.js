7ed22284f829ea4bb1ce3930d9375900a0368be1
Sampath Surineni <sampath.surineni@gmail.com>
2026-07-26 07:44:25 +0530

    `Update constructor and accessor example for BankAccount`

--- src/oop-practice/examples/2-3-constructor-accessors.js
+++ src/oop-practice/examples/2-3-constructor-accessors.js
@@ -1,28 +1,28 @@
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+function BankAccount(owner, balance = 0) {
+  this.owner = owner;
+  this.balance = balance;
 }
 
-Object.defineProperty(Person.prototype, 'name', {
+Object.defineProperty(BankAccount.prototype, 'accountInfo', {
   get() {
-    return `${this.firstName} ${this.lastName}`;
+    return `${this.owner}: $${this.balance}`;
   },
   set(value) {
     if (typeof value !== 'string') return;
-    const [firstName, ...lastNameParts] = value.trim().split(/\s+/);
-    if (!firstName || lastNameParts.length === 0) return;
-    this.firstName = firstName;
-    this.lastName = lastNameParts.join(' ');
+    const match = value.match(/^(.+?):\s*\$?(\d+)$/);
+    if (!match) return;
+    this.owner = match[1];
+    this.balance = Number(match[2]);
   },
   enumerable: false,
   configurable: true,
 });
 
-const person = new Person('Tony', 'Stark');
-person.name = 'Reed van Richards';
-const descriptor = Object.getOwnPropertyDescriptor(Person.prototype, 'name');
+const ashaAccount = new BankAccount('Asha', 1000);
+ashaAccount.accountInfo = 'Asha: $1500';
+const descriptor = Object.getOwnPropertyDescriptor(BankAccount.prototype, 'accountInfo');
 
 console.log('--- Constructor Prototype Accessor ---');
-console.log('name:', person.name);
-console.log('own accessor:', Object.hasOwn(person, 'name'));
+console.log('accountInfo:', ashaAccount.accountInfo);
+console.log('own accessor:', Object.hasOwn(ashaAccount, 'accountInfo'));
 console.log('enumerable:', descriptor.enumerable);
