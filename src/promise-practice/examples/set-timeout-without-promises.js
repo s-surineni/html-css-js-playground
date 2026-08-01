9e8f1fee56a6feccd02077b5610e9b7d5da60493
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-01 09:08:16 +0530

    Added async examples for setTimeout.

--- src/promise-practice/examples/set-timeout-without-promises.js
+++ src/promise-practice/examples/set-timeout-without-promises.js
@@ -21,3 +21,34 @@ function runAfterTimeout(callback) {
 runAfterTimeout(() => {
   console.log('4. After the second timeout');
 });
+
+function asyncExample(callback) {
+    setTimeout(() => {
+        console.log("inside set timeout");
+        callback()
+    }, 10)
+
+
+}
+
+function waitForTiemout() {
+    console.log("outside setttimeout");
+}
+asyncExample(waitForTiemout)
+
+function asyncExample2() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log("inside set timeout");
+            resolve()
+        }, 10)
+    })
+
+}
+
+function waitForTimeout2() {
+    const prom = asyncExample2();
+    prom.then(()=> console.log("after set timeout"));
+}
+
+waitForTimeout2()
