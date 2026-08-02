b21d2d074d9b8d588a6ccad5dff5a86c08491167
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-02 07:16:18 +0530

    Added todo API call to resolveSoon function.

--- src/promise-practice/examples/1-1-creating-and-resolving-without-promises.js
+++ src/promise-practice/examples/1-1-creating-and-resolving-without-promises.js
@@ -1,12 +1,22 @@
-function resolveSoon(value, callback) {
-  queueMicrotask(() => callback(value));
+function resolveSoon(todoId, callback) {
+  fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((todo) => callback(null, todo))
+    .catch((error) => callback(error));
 }
-
-const callback = (value) => {
-  console.log('resolved value:', value);
-};
 
 console.log('callback registered');
 console.log('waiting for callback...');
 
-resolveSoon('hello', callback);
+resolveSoon(1, (error, todo) => {
+  if (error) {
+    console.error('unable to load todo:', error.message);
+    return;
+  }
+  console.log('resolved value:', todo.title);
+});
