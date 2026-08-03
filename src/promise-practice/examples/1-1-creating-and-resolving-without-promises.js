6210694393a2df13822433a67945f35f82f4f023
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-03 05:48:16 +0530

    `Refactored resolveSoon to use a promise`

--- src/promise-practice/examples/1-1-creating-and-resolving-without-promises.js
+++ src/promise-practice/examples/1-1-creating-and-resolving-without-promises.js
@@ -27,22 +27,19 @@ console.log('outside async call')
 
 
 
-function resolveSoon() {
+function resolveSoonWithPromise() {
+  new Promise((resolve, reject) => {})
   needle.get(`https://official-joke-api.appspot.com/random_joke`, (error, response, body) => {
     if (error) {
-      callback(error);
       return;
     }
     if (response.statusCode < 200 || response.statusCode >= 300) {
-      callback(new Error(`HTTP ${response.statusCode}: ${response.statusMessage}`));
       return;
     }
     try {
       console.log("inside async");
       console.log(body);
-      callback()
     } catch (err) {
-      callback(err);
     }
   });
 }
