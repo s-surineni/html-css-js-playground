9b31820694fcf5c7d40b38f386a81047f08d8a9c
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-03 05:57:21 +0530

    improved promise examples

--- src/promise-practice/examples/1-1-creating-and-resolving-without-promises.js
+++ src/promise-practice/examples/1-1-creating-and-resolving-without-promises.js
@@ -1,35 +1,9 @@
-import needle from 'needle';
-
-function resolveSoon(callback) {
-  needle.get(`https://official-joke-api.appspot.com/random_joke`, (error, response, body) => {
-    if (error) {
-      callback(error);
-    }
-    else {
-      callback(body)
-    }
-  });
-}
+import { resolveSoon, resolveSoonWithPromise } from './1-1-needle-utils.js';
 
 resolveSoon(() => {
   console.log('after request in the callback')
 })
 console.log('outside async call')
-
-
-
-function resolveSoonWithPromise() {
-  return new Promise((resolve, reject) => {
-    needle.get(`https://official-joke-api.appspot.com/random_joke`, (error, response, body) => {
-      if (error) {
-        reject(error)
-      }
-      else {
-        resolve(body)
-      }
-    })
-  })
-}
 
 const promise = resolveSoonWithPromise()
 promise.then((result) => {
