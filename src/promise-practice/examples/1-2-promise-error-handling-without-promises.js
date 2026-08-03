156c809fd355b0171121a0f1156149a300a70717
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-03 06:00:16 +0530

    `Removed promise-less error handling example`

--- src/promise-practice/examples/1-2-promise-error-handling-without-promises.js
+++ src/promise-practice/examples/1-2-promise-error-handling-without-promises.js
@@ -1,30 +1,3 @@
-import needle from 'needle';
-
-export function resolveSoon(callback) {
-  needle.get(`https://official-joke-api.appspot.com/rando_joke`, (error, response, body) => {
-    if (error) {
-      callback(error);
-    }
-    else {
-      callback(body)
-    }
-  });
-}
-
-export function resolveSoonWithPromise() {
-  return new Promise((resolve, reject) => {
-    needle.get(`https://official-joke-api.appspot.com/random_joke`, (error, response, body) => {
-      if (error) {
-        reject(error)
-      }
-      else {
-        resolve(body)
-      }
-    })
-  })
-}
-
-
 function saveProfile(profile, callback) {
   queueMicrotask(() => {
     if (!profile.name?.trim()) {
