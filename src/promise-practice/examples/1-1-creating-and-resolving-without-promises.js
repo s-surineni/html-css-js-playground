0dc2ecbab93dd24d1148603663c549e6dd275ee6
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-03 05:46:16 +0530

    Added async request to example 1-1

--- src/promise-practice/examples/1-1-creating-and-resolving-without-promises.js
+++ src/promise-practice/examples/1-1-creating-and-resolving-without-promises.js
@@ -21,5 +21,33 @@ function resolveSoon(callback) {
 }
 
 resolveSoon(()=> {
-  console.log('after request')
+  console.log('after request in the callback')
 })
+console.log('outside async call')
+
+
+
+function resolveSoon() {
+  needle.get(`https://official-joke-api.appspot.com/random_joke`, (error, response, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    if (response.statusCode < 200 || response.statusCode >= 300) {
+      callback(new Error(`HTTP ${response.statusCode}: ${response.statusMessage}`));
+      return;
+    }
+    try {
+      console.log("inside async");
+      console.log(body);
+      callback()
+    } catch (err) {
+      callback(err);
+    }
+  });
+}
+
+resolveSoon(()=> {
+  console.log('after request in the callback')
+})
+console.log('outside async call')
