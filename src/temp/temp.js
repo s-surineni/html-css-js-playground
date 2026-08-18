4abdad872a1038081a8cd441193c5457c502429d
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-18 12:01:00 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -1,35 +1,5 @@
-class BankAccount {
-    constructor(name, balance) {
-        this.name = name
-        this.balance = balance
-    }
+const needle = require("needle");
 
-     getBalance() {
-        return this.balance
-    }
-
-    deposit(amount) {
-        this.balance += amount;
-    }
-
-    withdraw(amount) {
-        this.balance -= amount
-    }
-}
-
-const myAccount = new BankAccount('Mine', 500);
-const dAccount = new BankAccount('Dol', 50000);
-
-console.log('ironman myAccount.name', JSON.stringify(myAccount.name));
-console.log('ironman myAccount.balance', JSON.stringify(myAccount.balance)); // undefined
-myAccount.balance = 100;
-console.log('ironman myAccount.getBalance()', JSON.stringify(myAccount.getBalance()));
-
-myAccount.deposit(100);
-console.log('ironman myAccount.getBalance()', JSON.stringify(myAccount.getBalance()));
-
-myAccount.withdraw(200);
-console.log('ironman myAccount.getBalance()', JSON.stringify(myAccount.getBalance()));
-
-console.log('ironman dAccount.name', JSON.stringify(dAccount.name));
-console.log('ironman dAccount.getBalance()', JSON.stringify(dAccount.getBalance()));
+console.log('ironman before promise');
+function getJoke() {
+    needle
