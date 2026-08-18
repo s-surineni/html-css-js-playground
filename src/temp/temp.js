99ecb92c67d0336d1719833bd32313ef8fdd78f3
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-19 05:10:54 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -1,5 +1,21 @@
-const needle = require("needle");
+import needle from 'needle';
+
 
 console.log('ironman before promise');
+export const JOKE_URL = 'https://official-joke-api.appspot.com/random_joke';
 function getJoke() {
-    needle.get
+    return new Promise((resolve, reject) => {    needle.get(JOKE_URL, (err, response, body) => {
+        if (err) {
+            reject(err)
+        } else {
+            resolve(body)
+        }
+    })})
+
+}
+const prom = getJoke()
+prom.then((res)=>{
+    console.log(res)
+    console.log('after then promise')
+})
+console.log('afterThen')
