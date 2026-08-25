0985743c9e545c980790516371ae4134e7487b66
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-25 15:11:06 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -1,34 +1,5 @@
-import needle from 'needle';
-
-
-console.log('ironman before promise');
-export const JOKE_URL = 'https://official-joke-api.appspot.com/random_joke';
- function getJoke() {
-    return new Promise((resolve, reject) => {    needle.get(JOKE_URL, (err, response, body) => {
-        if (err) {
-            reject(err)
-        } else {
-            resolve(body)
-        }
-    })})
-
+class BankAccount {
+    constructor (name, balance) {
+        thi
+    }
 }
-
-async function getJokeAsyn() {
-     new Promise((resolve, reject) => {    needle.get(JOKE_URL, (err, response, body) => {
-        if (err) {
-            reject(err)
-        } else {
-            resolve(body)
-        }
-    })})
-
-}
-try {
-    const res = await getJokeAsyn()
-    console.log(res)
-    console.log('after await')
-} catch (err) {
-    console.error('joke failed:', err.message)
-}
-console.log('afterAwait')
