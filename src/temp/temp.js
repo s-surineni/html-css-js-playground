2eb14a7f4ec0561464eeccab81dbbb9cbab9b48b
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-25 15:21:02 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -1,5 +1,12 @@
 class BankAccount {
-    constructor (name, balance) {
-        thi
+    name;
+    balance;
+
+    constructor(name, balance) {
+        this.name = name;
+        this.balance = balance;
     }
 }
+
+const b1 = new BankAccount('d1', 10000)
+console.log(b1.name)
