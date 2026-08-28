f78bdfa41347df82c58a53370d03a555b4c8469c
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-28 05:40:51 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -1,3 +1,9 @@
-import needle from "needle";
+import needle from 'needle';
 
-needle.get()
+needle.get('https://jsonplaceholder.typicode.com/users/1', (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(body);
+});
