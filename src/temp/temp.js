6467ad120c620cd7867f6fd7b1276fe30677f39f
Sampath Surineni <sampath.surineni@gmail.com>
2026-08-28 06:00:53 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -1,13 +1,15 @@
-import needle from 'needle';
+import needle from 'needle'
 
 console.log('before promise')
-new Promise((resolve, reject) => {
-    needle.get('https://jsonplaceholder.typicode.com/users/1', (error, resp, body) => {
-        console.log('error', error)
-        // console.log('resp', resp)
-        console.log('body', body)
-        console.log('after request')
-    });
+const prom = new Promise((resolve, reject) => {
+  needle.get('https://jsonplaceholder.typicode.com/users/1', (error, resp, body) => {
+    if (error) {
+      reject(error)
+    } else {
+      resolve(body)
+    }
+  })
 })
-
+const body = await prom
+console.log('body', body)
 console.log('after promise')
