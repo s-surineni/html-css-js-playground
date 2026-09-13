57c4a607a019390336375d8c09ccba98d1360123
Sampath Surineni <sampath.surineni@gmail.com>
2026-09-13 06:00:52 +0530

    Auto-sync from ssurineni@ssurine-ltm3c4c.internal.salesforce.com

--- src/temp/temp.js
+++ src/temp/temp.js
@@ -1,16 +1,31 @@
-class BankAccount {
-  #balance
-  constructor(name, balance) {
-    this.name = name
-    this.#balance = balance
-  }
-  get balance() {
-    return this.#balance;
-  }
-    deposit(amount) {
-    this.#balance += amount;
-  }
+// class BankAccount {
+//   #balance
+//   constructor(name, balance) {
+//     this.name = name
+//     this.#balance = balance
+//   }
+//   get balance() {
+//     return this.#balance;
+//   }
+//     deposit(amount) {
+//     this.#balance += amount;
+//   }
+// }
+// const b1 = new BankAccount('dd', 1000)
+// console.log(b1.balance)
+// b1.deposit(1000)
+// console.log(b1.balance)
+
+
+function BankAccount(name, balance){
+  this.name = name;
+  this.balance = balance;
+
+  this.deposit = function deposit(amount) {
+    this.balance += amount;
+  };
 }
+
 const b1 = new BankAccount('dd', 1000)
 console.log(b1.balance)
 b1.deposit(1000)
